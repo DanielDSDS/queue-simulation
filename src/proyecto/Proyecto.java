c297b069d399ca2fe1ef2b06411d8d9e4d9e15c6
Daniel Di Mella <bigbigbyned@gmail.com>
2021-07-15 20:30:01 -0400

    Initial queue simulator commit

---
+++ src/proyecto/Proyecto.java
@@ -0,0 +1,23 @@
+
+package proyecto;
+
+import Vistas.Parametros;
+
+
+/**
+ *
+ * @author Labam
+ */
+public class Proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Parametros P = new Parametros();
+        P.setVisible(true);
+        
+    }
+    
+}
