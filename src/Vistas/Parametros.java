3dbc7725ddb0b1bddc4baaa5823486be6ff51138
Daniel Di Mella <bigbigbyned@gmail.com>
2021-07-28 11:20:25 -0400

    Mover boton solo por pruebas

--- src/Vistas/Parametros.java
+++ src/Vistas/Parametros.java
@@ -281,7 +281,7 @@ public class Parametros extends javax.swing.JFrame {
       
         simular.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         simular.setText("Simular");
-        jPanel1.add(simular, new org.netbeans.lib.awtextra.AbsoluteConstraints(265, 790, 120, 30));
+        jPanel1.add(simular, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 620, 120, 30));
         simular.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 simularActionPerformed(evt);
@@ -445,7 +445,7 @@ public class Parametros extends javax.swing.JFrame {
                                         this.tablaLlegadasA,
                                         this.tablaServidoresA,
                                         this.salida);
-            Simulacion.comenzarSimulacion();
+            Simulacion.iniciarSimulacion();
             this.setVisible(false);
             salida.setVisible(true);
     }//GEN-LAST:event_siguienteActionPerformed
